154e5450a7138cdae30b5fc2316c9e3c2dc1568d
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2014-07-16 20:08:33 -0400

    update pour le clearDisplay(voir note ci-bas) note: la methode d'executer les commandes cls ou clear est un mythe, ça ne fonctionne que dans les languages c. .exec() en java appel la commande dans une autre console, ce qui est inutile. Il existe une façon de le faire en ANSI mais ANSI foncitonne seulement sur un terminal UNIX. La seule vrai façon portable est juste de passer plusieurs lignes. Donc ce sera la solution utilisée jusqu'à ce que l'on développe une interface.

--- Rogue232/src/Game/GraphicEngine.java
+++ Rogue232/src/Game/GraphicEngine.java
@@ -1,39 +1,29 @@
 package Game;
 
 public class GraphicEngine{
-	private String worldString;
 	
 	public void updateDisplay(World world, Character hero){
-		toStringWorld(world, hero);
+		String s = toStringWorld(world, hero);
 		clearConsole();
-		System.out.println(worldString);
+		System.out.println(s);
 	}
 	
 	private void clearConsole(){
-		try
-		{
-			final String os = System.getProperty("os.name");
-			if (os.contains("Windows")){
-				Runtime.getRuntime().exec("cls");
-			} else {
-				Runtime.getRuntime().exec("clear");
-			}
-		}
-		catch (final Exception e){
-			;
-		}
+		//note: c'est la seule facon portable de le faire en java
+		for (int i = 0; i < 50; ++i) System.out.println();
 	}
 	
-	private void toStringWorld(World world, Character hero){
+	private String toStringWorld(World world, Character hero){
 		
 		char posHeroChar = world.getChar(hero.getPosition());
 		world.setChar(hero.getPosition(), hero.getSymbol());
 		
-		worldString = "";
+		String worldString = "";
 		for(char[] str : world){
 			worldString += new String(str) + System.getProperty("line.separator");
 		}
 		
 		world.setChar(hero.getPosition(), posHeroChar);
+		return worldString;
 	}
 }
